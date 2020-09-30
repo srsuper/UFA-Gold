96b47fc1561ee52f75d3b498741e1e0de3a46fac
xkritti <krittamet0404@gmail.com>
2020-10-01 02:17:07 +0700

    DesignPositionOfPageLogin

--- src/component/login.js
+++ src/component/login.js
@@ -1,60 +1,135 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Row, Container, Button, Card, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
-
+import { Col, Row, Container, Button, Card, Input, InputGroup, InputGroupAddon, InputGroupText, Label } from 'reactstrap';
+import LoginCard from './logincard'
 
 const Login = () => {
-    const [loginlog, setLoginlog] = useState({
-        id: 'Username',
-        pass: 'Password'
-    })
-
-    // useEffect(() => {
-
-    // }, [loginlog])
 
     return (
-        <div>
-            <Container>
-                <Col>
-                    <Row >
-                        <div style={{ width: "80%" }}>
-                            <h1>UFA_Gold</h1>
-                        </div>
-                        <Row>
-                            <Card style={{ alignItems: 'center', marginTop: '50px', padding: "20px" }}>
-                                <h1>LOGIN UFABET</h1>
-                                <form style={{ marginBottom: "10px", marginTop: '10px' }} method="post" action="https://www.ufa6666.com/Default8.aspx?lang=EN-GB">
-                                    <input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="btnLogin" ></input>
-                                    <input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value></input>
-                                    <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUKLTc2NjMxOTE5Nw9kFgJmD2QWBgICDxYCHgtwbGFjZWhvbGRlcgUe4LiK4Li34LmI4Lit4Lic4Li54LmJ4LmD4LiK4LmJZAIDDxYCHwAFGOC4o+C4q+C4seC4quC4nOC5iOC4suC4mWQCBA8PFgIeBFRleHQFIeC5gOC4guC5ieC4suC4quC4ueC5iOC4o+C4sOC4muC4mmRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQlidG5Mb2dpbjK0GvGzmtq4/Y17MirZEaYkr5WZJg=="></input>
-                                    <input type="hidden" type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="BE6BC141"></input>
-                                    <Card>
-                                        <InputGroup>
-                                            <InputGroupAddon addonType="prepend">
-                                                <InputGroupText style={{ width: "100px" }}>Username :</InputGroupText>
-                                            </InputGroupAddon>
-                                            <Input id='textUserName' name="txtUserName" type="text" placeholder="USERNAME" required />
-                                        </InputGroup>
-                                        <InputGroup>
-                                            <InputGroupAddon addonType="prepend">
-                                                <InputGroupText style={{ width: "100px" }} >Password :</InputGroupText>
-                                            </InputGroupAddon>
-                                            <Input id="password" name="password" type="password" placeholder="PASSWORD" required />
-                                        </InputGroup>
-                                        <Button color="danger" type="submit">เข้าสุ่ระบบ</Button>
-                                    </Card>
-                                </form>
-                                <Button color="success" style={{ width: "150px" }}>Add Friends</Button>
-                                <span>PARTNER OF UFABET.COM</span>
-                            </Card>
+        <div style={{ backgroundColor: 'black' }} >
+            <Container fluid={true} style={{ maxWidth: '100vh' }}>
+                <Row style={{ backgroundColor: 'black', height: '23vh' }}>
+                    <Col style={{ backgroundColor: 'red' }}>
+                        <h1>Header</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{ backgroundColor: 'blue', height: '100%' }}>
+                        <Row style={{ flex: 'display', alignItems: 'center', justifyContent: 'center', backgroundColor: 'gold', height: '20vh' }}>
+                            <Col xs="4" >
+                                <Card style={{ height: '13vh' }}>
+                                    <h1>
+                                        Game 1
+                                    </h1>
+                                </Card>
+                            </Col>
+                            <Col xs="4">
+                                <Card style={{ height: '13vh' }}>
+                                    <h1>
+                                        Game 2
+                                    </h1>
+                                </Card>
+                            </Col>
+                            <Col xs="4">
+                                <Card style={{ height: '13vh' }}>
+                                    <h1>
+                                        Game 3
+                                    </h1>
+                                </Card>
+                            </Col>
+
                         </Row>
+                        <Row >
+                            <Col sm="8" style={{ backgroundColor: 'brown' }}>
+                                <h1>
+                                    เนื้อหา
+                            </h1>
+                                <Card style={{ marginBottom: '10px' }}>
+                                    <h3 style={{ margin: '20px', marginBottom: '5px' }}>
+                                        แทงบอลออนไลน์ UFABET ยูฟ่าเบท | UFABET.COM
+                                    </h3>
+                                    <Label style={{ margin: '20px' }}>
+                                        ตัวแทนที่ได้รับสิขสิทธิ์อย่างเป็นทางการจาก UFABET ยูฟ่าเบท เว็บแทงบอลออนไลน์ ที่ได้มาตรฐานระดับโลกที่ดี
+                                        ที่สุดในประเทศไทย ท่านสามารถเดิมพันกีฬาออนไลน์ แทงบอลออนไลน์ บอลสเต็ป บอลชุด<br /> บาคาร่า คาสิโนออนไลน์
+                                    หวย มวย ได้ทุกชนิดโดยเปิดราคามากกว่าเจ้าอื่น สะดวกสบายยิ่งขึ้นสามารถวางเดิมพันได้ทุกที่ทุกเวลา
+                                    รองรับมือถือทุกรุ่นทุกระบบ และที่สำคัญเป็นภาษาไทยทั้งระบบ<br />
+                                        <br />
 
-                    </Row>
-
-                </Col>
-
-            </Container>
-        </div>
+                                    เว็บไซต์ยูฟ่าเบท UFABET มีความปลอดภัยมาตรฐานระดับโลก การป้องกันข้อมูลมากกว่า 10 ชั้น ทำให้ท่านลูกค้า
+                                    ไม่ต้องกังวลกับความปลอดภัยด้านข้อมูลส่วนตัว ที่รองรับการพนันบอลออนไลน์ ผ่านอินเตอร์เน็ต และคาสิโน
+                                    ออนไลน์ที่มีให้เลือกมากกว่า 1000 เกมส์ พร้อมทั้งยังมีคาสิโนที่ถ่ายทอดสดตลอด 24 ชั่วโมง พร้อมโปรโมชั่นมากมาย
+                                    UFABET มีความยินดีให้บริการทุกท่านตลอด 24 ชั่วโมง กับทีมงานมืออาชีพพร้อมแก้ไขปัญหาและรับใช้
+                                    ทุกท่านในการฝากถอนภายใน 5 นาที <br />
+                                    </Label>
+                                </Card>
+                                <Card style={{ marginBottom: '10px', height: '18vh' }}>
+                                    <span>
+                                        ขั้นตอน
+                                    </span>
+                                </Card>
+                                <Card style={{ marginBottom: '10px' }}>
+                                    <h3 style={{ margin: '20px', marginBottom: '5px' }}>
+                                        UFABETCN.COM (UFABET) พนันและแทงบอลออนไลน์ที่ดีที่สุด
+                                    </h3>
+                                    <Label style={{ margin: '20px' }}>
+                                        UFABET ในปัจจุบันนั้นเว็บพนันออนไลท์นั้นเกิดขึ้นอย่างแพร่หลาย และมีมากมายไห้ลูกค้าได้เลือกหาที่จะเล่นกัน แต่วันนี้เราจะมาแนะนำเว็บไซค์น้องใหม่ล่าสุดสดๆ ร้อนๆ นั้นก็คือ
+                                        เว็บไซต์น้องใหม่มาแรงแซงทุกเว็บ ของยุคนี้ สีสันสดใส เข้าใจง่าย ตัวหนังสือชัดเจน รองรับหลายภาษา รวมถึงภาษาไทย เรียบง่ายไม่ยุ่งยาก คัดสรรค์กีฬามามากมาย <br />
+                                        <br />
+                                        ไม่ว่าจะเป็น ฟุตบอล บาสเกตบอล เทนนิส วอลเล่ย์บอล แม้แต่แบตมินตันก็ยังมีไห้ลูกค้าได้เสี่ยงโชคกัน ในส่วนของฟุตบอลนั้นมีฟุตบอลๆหลากหลายประเทศ ลีคต่างๆ มีไห้เล่นกันเยอะมาก
+                                        ในส่วนของฟุตบอลไทยนั้นทางเว็บก็ยังใส่ไว้ไห้ลูกค้าได้เล่นกัน ลูกค้าจะพบกับการเดิมพันหลากหลายทำไห้คุณเพลิดเพลินและยังได้รับส่วนลดการเดิมพันซึ่งจะทำไห้ได้รับเงินเป็นจำนวนเงินที่มากขึ้นกว่าการเดิมพันที่เกิดขึ้นจริง<br />
+                                        <br />
+                                        การแทงบอลออนไลน์สามารถเลือกการแทงได้หลายแบบ เช่น การแทงแบบราคาแฮนดิแคป แทงสกอร์สูงต่ำ แทงจำนวนแตะมุม และการแทงใบเหลืองใบแดง สามาถเล่นบอลสดได้ตลอด 90 นาทีทำให้ลูกค้าได้รับความได้เปรียบในการแทงบอลออนไลน์เป็นอย่างมาก โดยที่ UFABET (ยูฟ่าเบท) ได้เปิดอัตราต่อรองทุกรูปแบบทุกราคาต่อรอง<br />
+                                    </Label>
+                                    <h3 style={{ margin: '20px', marginBottom: '5px' }}>
+                                        สมัครยูฟ่าเบท พร้อมให้บริการ UFA เว็บพนันออนไลน์ 24 ชั่วโมง 365 วัน
+                                    </h3>
+                                    <Label style={{ margin: '20px' }}>
+                                        ยูฟ่าเบท เว็บให้บริการพนันออนไลน์ ที่ได้รับการยอมรับจากทั่วโลก เราพร้อมให้บริการเว็บพนันออนไลน์ คุณสามารถสมัครยูฟ่าเบท เพื่อเข้าใช้บริการระบบต่างๆ อาทิเช่น บาคาร่ายูฟ่า และ ยูฟ่าสล็อต  คุณสามารถสมัครยูฟ่า กับทางทีมงาน UFA เว็บตรงไม่ผ่านเอเย่นต์ได้หลากหลายช่องทาง เช่น ทางหน้าเว็บไซต์ โดยกรอกรายละเอียดการสมัครด้วยตัวเอง<br />
+                                        <br />
+                                        เพื่อความสะดวกของและป้องกันปัญหาการใช้งาน เรายังมีช่องทางการเข้าใช้งาน UFA ที่คอยอัพเดท ทางเข้ายูฟ่าเบท ที่ช่วยให้สมาชิกที่สมัครเข้ามา สามารถเข้าเล่นได้ตลอด 24 ชั่วโมง 365 วัน โดยไม่ติดขัด ลิ้งเข้าเว็บไซต์ ufabet หลัก เช่น www.ufabet.com, www.betufa.com และ
+                                        ลิ้งเข้าระบบ ufabet รองอื่นๆอีกมากมาย เช่น www.ufabet9999.com, www.ufabet7777.com, www.ufabet6666.com เป็นต้น<br />
+                                        <br />
+                                        สมัคร ufabet ออนไลน์ กับเว็บไซต์ยอดนิยม เช่น ยูฟ่าเบท88888, ยูฟ่าเบท168, ยูฟ่าเบท365, ยูฟ่าเบท777, ยูฟ่า191 และ ยูฟ่า168
+                                        หรือคุณสามารถ สมัครยูฟ่า เพื่อเล่น คาสิโนออนไลน์ บาคาร่า สล๊อต แทงบอลออนไลน์ หรือ กีฬาอื่นๆ www.ufabet.com เริ่มเดิมพัน พันออนไลน์ มั่นคงปลอดภัย<br />
+                                    </Label>
+                                    <h3 style={{ margin: '20px', marginBottom: '5px' }}>
+                                        สมัครบาคาร่า 888 ufabet สมัครเว็บบาคาร่า 1688
+                                    </h3>
+                                    <Label style={{ margin: '20px' }}>
+                                        สมัครบาคาร่า เกมส์คาสิโนออนไลน์ กับ ufabet.com เว็บคาสิโนยอดนิยมที่ได้รับการยอมรับจากทั่วโลก ถ้าคุณเคย สมัครเว็บบาคาร่า อื่นๆมาแล้ว อาทิเช่น สมัคร Gclub ก็ไม่ยากเลยที่จะลองมา สมัครเล่นบาคาร่า กับยูฟ่าเบท ซึ่งมีเกมส์ไพ่ในรูปแบบต่างๆให้ได้เลือกเล่นมากมาย เช่น Sexy Baccarat ซึ่งคนแจกไพ่จะแต่งตัวเซ็กซี่ และมีท่าเต้นที่สนุกสนานช่วยเพิ่มความเพลิดเพลินและเพิ่มสีสันในการเล่น<br />
+                                        <br />
+                                    </Label>
+                                    <h3 style={{ margin: '20px', marginBottom: '5px' }}>
+                                        สมัคร ufabet เว็บแทงบอล vip 888 โปรโมชั่น ดี คืนยอดเสีย ทุกเดือน
+                                    </h3>
+                                    <Label style={{ margin: '20px' }}>
+                                        นอกจากนี้เว็บ ufabetcn ยังเป็นตัวแทนที่ได้รับการแต่งตั้งจาก UFABET เช่นเดียวกับเว็บอื่นๆในเครือเช่น UFABET365, UFA168, UFABET168, UFA1688, UFA888, UFA69, UFABET888, UFA678, UFABET777, UFABET888, UFA9999, UFA7777, UFA191 เป็นต้น เรามีโปรโมชั่น ufabet คืนยอดเสีย 5% ทุกวันที่ 1 ของทุกเดือน <br />
+                                    </Label>
+                                </Card>
+                            </Col>
+                            <Col xs="4" style={{ backgroundColor: 'crimson' }}>
+                                <LoginCard />
+                            </Col>
+                        </Row>
+                        <Col>
+                            <Row style={{ height: '35vh', backgroundColor: 'darkgoldenrod' }}>
+                                <h1>Promotion 1</h1>
+                            </Row>
+                            <Row style={{ height: '35vh', backgroundColor: 'darkred' }}>
+                                <h1>Promotion 2</h1>
+                            </Row>
+                            <Row style={{ height: '35vh', backgroundColor: 'deeppink' }}>
+                                <h1>Promotion 3</h1>
+                            </Row>
+                        </Col>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{ backgroundColor: 'green' }}>
+                        <h1>Footer</h1>
+                    </Col>
+                </Row>
+            </Container >
+        </div >
     )
 }
 
---
+++ src/component/logincard.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Col, Row, Container, Button, Card, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
+
+
+const logincrad = () => {
+
+    return (
+        <div>
+            <Card style={{ alignItems: 'center', marginTop: '10px', marginBottom: '10px'}}>
+                <h1>LOGIN UFABET</h1>
+                <form style={{ marginBottom: "10px", marginTop: '10px' }} method="post" action="https://www.ufa6666.com/Default8.aspx?lang=EN-GB">
+                    <input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="btnLogin" ></input>
+                    <input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value></input>
+                    <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUKLTc2NjMxOTE5Nw9kFgJmD2QWBgICDxYCHgtwbGFjZWhvbGRlcgUe4LiK4Li34LmI4Lit4Lic4Li54LmJ4LmD4LiK4LmJZAIDDxYCHwAFGOC4o+C4q+C4seC4quC4nOC5iOC4suC4mWQCBA8PFgIeBFRleHQFIeC5gOC4guC5ieC4suC4quC4ueC5iOC4o+C4sOC4muC4mmRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQlidG5Mb2dpbjK0GvGzmtq4/Y17MirZEaYkr5WZJg=="></input>
+                    <input type="hidden" type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="BE6BC141"></input>
+                    <Card>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText style={{ width: "100px" }}>Username :</InputGroupText>
+                            </InputGroupAddon>
+                            <Input id='textUserName' name="txtUserName" type="text" placeholder="USERNAME" required />
+                        </InputGroup>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText style={{ width: "100px" }} >Password :</InputGroupText>
+                            </InputGroupAddon>
+                            <Input id="password" name="password" type="password" placeholder="PASSWORD" required />
+                        </InputGroup>
+                        <Button color="danger" type="submit">เข้าสุ่ระบบ</Button>
+                    </Card>
+                </form>
+                <Button color="success" style={{ width: "150px" }}>Add Friends</Button>
+                <span>PARTNER OF UFABET.COM</span>
+            </Card>
+        </div>
+    )
+}
+
+export default logincrad;
